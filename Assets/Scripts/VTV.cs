b6965830a9e39c411de0d48b3b6880369092a000
facujarma <49127963@est.ort.edu.ar>
2025-04-29 08:27:28 -0300

    Examen Terminado

---
+++ Assets/Scripts/VTV.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VTV : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string patente;
+    public int modelo;
+    public int kmRecorridos;
+    public int vencimientoActual;
+    public int HCppm;
+
+    int anioActual = 2025;
+    void Start()
+    {
+        if( (patente == "") || (modelo < 1900 || modelo > anioActual ) || (kmRecorridos < 0) || (vencimientoActual >= anioActual || vencimientoActual <= modelo) || (HCppm < 5))
+        {
+            Debug.Log("VTV no aprobada");
+            return;
+        }
+        if(HCppm > 100)
+        {
+            Debug.Log("VTV no aprobada: Los hidrorcarburos partículas por millón superan el maximo (100)");
+            return;
+        }
+
+        int promedioKmRecorridos = kmRecorridos / (anioActual - modelo);
+        if (promedioKmRecorridos <= 10000)
+        {
+            Debug.Log("VTV aprobada por 2 años");
+        }
+        else
+        {
+            Debug.Log("VTV aprobada por 1 año");
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
