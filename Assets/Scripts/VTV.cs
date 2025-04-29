32392fb3699f3d79dad1580bf52192b146dc44ed
facujarma <49127963@est.ort.edu.ar>
2025-04-29 08:35:26 -0300

    Terminado v2

--- Assets/Scripts/VTV.cs
+++ Assets/Scripts/VTV.cs
@@ -14,12 +14,33 @@ public class VTV : MonoBehaviour
     int anioActual = 2025;
     void Start()
     {
-        if( (patente == "") || (modelo < 1900 || modelo > anioActual ) || (kmRecorridos < 0) || (vencimientoActual >= anioActual || vencimientoActual <= modelo) || (HCppm < 5))
+        if( (patente == ""))
         {
-            Debug.Log("VTV no aprobada");
+            Debug.Log("VTV no aprobada: Ingresa un patente valida");
             return;
         }
-        if(HCppm > 100)
+        if(modelo < 1900 || modelo > anioActual)
+        {
+            Debug.Log("VTV no aprobada: El año de fabricacion es menor a 1900 o mayor al año actual");
+            return;
+        }
+        if (kmRecorridos < 0)
+        {
+            Debug.Log("VTV no aprobada: Los kilometros recorridos deben ser mayor o iguales a cero.");
+            return;
+        }
+        if (vencimientoActual >= anioActual || vencimientoActual <= modelo)
+        {
+            Debug.Log("VTV no aprobada: El vencimiento debe ser menor que el año actual y mayor que el año de fabricación.");
+            return;
+        }
+
+        if((HCppm < 5))
+        {
+            Debug.Log("VTV no aprobada: Los hidrorcarburos partículas por millón son menores que el minimo (5)");
+            return;
+        }
+        if (HCppm > 100)
         {
             Debug.Log("VTV no aprobada: Los hidrorcarburos partículas por millón superan el maximo (100)");
             return;
